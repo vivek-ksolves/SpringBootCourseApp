17d93c353541d3e0fe219629e3b4da1768c1ec63
vivek-ksolves <vivek.singh@ksolves.com>
2021-02-22 12:18:48 +0530

    My initial commit

---
+++ src/main/java/CourseApp/HelloController.java
@@ -0,0 +1,15 @@
+package CourseApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		
+		return "Hello from Vivek";
+	}
+
+}
